30c478dbf1f2db3626aa2bbd98e6f9288b94dfbb
michaelvargas19 <michael.vargas@javeriana.edu.co>
2018-10-10 18:52:52 -0500

    v0.4Routing

--- Source/FrontEndSupermercado/src/app/app.module.ts
+++ Source/FrontEndSupermercado/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
